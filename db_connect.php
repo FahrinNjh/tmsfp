cb316e2dcfe867301a866854d3a687a8617c656c
FahrinNjh <fahrinnajah1711@gmail.com>
2025-01-02 21:14:52 +0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,32 @@
-<?php 
+<?php
+  
+$host = "tmsfp.mysql.database.azure.com";       
+$username = "tmsfp";        
+$password = "Qwerty12345";            
+$database = "tms_db"; 
+$port = 3306;
 
-$conn= new mysqli('localhost','root','','tms_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+} else {
+    echo "Koneksi berhasil";
+}
+
+function executeQuery($query) {
+    global $conn;
+    $result = $conn->query($query);
+
+    if ($result === FALSE) {
+        die("Query Error: " . $conn->error);
+    }
+
+    return $result;
+}
+
+function closeConnection() {
+    global $conn;
+    $conn->close();
+}
+?>
